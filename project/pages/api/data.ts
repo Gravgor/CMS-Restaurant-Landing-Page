a03af114b0b7c1e093a073b810f27c16cdc99d8a
Gravgor <miloszleok@gmail.com>
2022-12-18 02:27:57 +0100

    Update /api/data

--- project/pages/api/data.ts
+++ project/pages/api/data.ts
@@ -8,22 +8,40 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         res.status(200).json(data);
     } else if (req.method === 'POST'){
         if(!req.headers.authorization) return res.status(401).json("Not authorized");
+        if(!req.body.title || !req.body.email || !req.body.phone || !req.body.address || !req.body.address2) return res.status(400).json("Bad request");
         const dataGet = await prisma.content.findMany();
-        const newPhone = req.body.phone.slice(0, 3) + ' ' + req.body.phone.slice(3, 6) + ' ' + req.body.phone.slice(6, 9)
-        const data = await prisma.content.update({
-            where: {
-                id: 1
-            },
-            data: {
-                title: req.body.title ? req.body.title : dataGet[0].title,
-                email: req.body.email ? req.body.email : dataGet[0].email,
-                phone: newPhone ? newPhone : dataGet[0].phone,
-                address: req.body.address ? req.body.address : dataGet[0].address,
-                address2: req.body.address2 ? req.body.address2 : dataGet[0].address2,
-            }
-        })
-        await prisma.$disconnect();
-        res.status(200).json(data);
+        if(req.body.phone === ''){
+            const newPhone = dataGet[0].phone;
+            const data = await prisma.content.update({
+                where: {
+                    id: 1
+                },
+                data: {
+                    title: req.body.title ? req.body.title : dataGet[0].title,
+                    email: req.body.email ? req.body.email : dataGet[0].email,
+                    phone: newPhone ? newPhone : dataGet[0].phone,
+                    address: req.body.address ? req.body.address : dataGet[0].address,
+                    address2: req.body.address2 ? req.body.address2 : dataGet[0].address2,
+                }
+            })
+            await prisma.$disconnect();
+            res.status(200).json(data);
+        }else{
+            const data = await prisma.content.update({
+                where: {
+                    id: 1
+                },
+                data: {
+                    title: req.body.title ? req.body.title : dataGet[0].title,
+                    email: req.body.email ? req.body.email : dataGet[0].email,
+                    phone: req.body.phone ? req.body.phone : dataGet[0].phone,
+                    address: req.body.address ? req.body.address : dataGet[0].address,
+                    address2: req.body.address2 ? req.body.address2 : dataGet[0].address2,
+                }
+            })
+            await prisma.$disconnect();
+            res.status(200).json(data);
+        }
     } else {
         res.status(405).end();
     }
